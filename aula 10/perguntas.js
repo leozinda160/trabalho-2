6941eddfc20967889a1459476f8cf3f481fc5e9b
leozinda160 <137521440+leozinda160@users.noreply.github.com>
2024-10-22 09:48:39 -0300

    Add files via upload

---
+++ aula 10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da inglaterra',
+    'Londres'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'curiosidade',
+    'Quem fez este trabalho',
+    'léo vitor'
+)
